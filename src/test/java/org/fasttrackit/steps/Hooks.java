b3684079bc1cd37fd457441980716995a5d4ec32
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-03-27 19:33:14 +0200

    studied cucumber reports, cucumber screenshots

--- src/test/java/org/fasttrackit/steps/Hooks.java
+++ src/test/java/org/fasttrackit/steps/Hooks.java
@@ -4,6 +4,8 @@ import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import org.fasttrackit.DriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 
 public class Hooks {
@@ -17,6 +19,15 @@ public class Hooks {
     @After
     public void tearDown(Scenario scenario) throws InterruptedException {
         Thread.sleep(500);
+
+//        scenario.write("Custom information...");
+
+        if (scenario.isFailed()) {
+            byte[] screenshot = ((TakesScreenshot) DriverManager.getDriver())
+                    .getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
+        }
+
         DriverManager.getDriver().quit();
     }
 }
